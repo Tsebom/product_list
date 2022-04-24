2daa691ae08b49d477ef032ee231c88e98ff08d9
Nezhdanov Sergei <tsebom@yandex.ru>
2022-04-25 01:16:16 +0300

    product list complite

---
+++ src/main/java/org/example/components/WelcomeController.java
@@ -0,0 +1,13 @@
+package org.example.components;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
